44ab59aa5375a63689877dba8b063b09544bd065
Jasper Watson <jasper.g.watson@gmail.com>
2019-09-23 14:00:06 +0000

    version 0.1.0

---
+++ README.md
@@ -0,0 +1,18 @@
+
+<!-- README.md is generated from README.Rmd. Please edit that file -->
+inverseRegex
+============
+
+The inverseRegex package allows users to reverse engineer regular expression patterns for R objects. Individual characters can be categorised into digits, letters, punctuation or spaces and encoded into run-lengths. This could be useful to summarise a dataset without viewing all individual entries or to aid in data cleaning.
+
+The vignette [vignettes/overview.Rmd](https://github.com/rntq472/inverseRegex/blob/master/vignettes/overview.Rmd) contains an introduction to the package.
+
+Installation
+------------
+
+You can install the development version of inverseRegex from GitHub with:
+
+``` r
+# install.packages("devtools")
+devtools::install_github("rntq472/inverseRegex")
+```
---
+++ inst/doc/overview.R
@@ -0,0 +1,50 @@
+## ----setup, include = FALSE----------------------------------------------
+knitr::opts_chunk$set(
+  collapse = TRUE,
+  comment = "#>"
+)
+
+## ---- echo = FALSE-------------------------------------------------------
+
+library(inverseRegex)
+
+
+## ---- echo = TRUE--------------------------------------------------------
+inverseRegex('1aA')
+inverseRegex('1aA', combineCases = TRUE)
+inverseRegex('1aA', combineAlphanumeric = TRUE)
+inverseRegex('Hello World!')
+inverseRegex('Hello World!', combineSpace = TRUE, combinePunctuation = TRUE)
+
+
+## ---- echo = TRUE--------------------------------------------------------
+inverseRegex('abcd1234567')
+inverseRegex('abcd1234567', numbersToKeep = NULL)
+inverseRegex('abcd1234567', numbersToKeep = 1:10)
+
+## ---- echo = TRUE--------------------------------------------------------
+vapply(c(1, 1.0, 1.10, 1.12, 1.123), format, character(1), nsmall = 1)
+inverseRegex(c(1, 1.0, 1.10, 1.12, 1.123), numbersToKeep = 2:10)
+
+## Vectors of class integer are just converted using as.character.
+inverseRegex(1L)
+
+
+## ---- echo = TRUE--------------------------------------------------------
+inverseRegex(c(1, 1.0, 1.10, 1.12, 1.123))
+inverseRegex(data.frame(a = c(1, 1.0, 1.10, 1.12, 1.123)))
+
+
+## ---- echo = TRUE--------------------------------------------------------
+unique(inverseRegex(iris, numbersToKeep = 2:10))
+
+
+## ---- echo = TRUE--------------------------------------------------------
+occurrencesLessThan(c(LETTERS, 1))
+
+## When called on a data frame occurrencesLessThan will assess each column individually.
+x <- iris
+x$Species <- as.character(x$Species)
+x[27, 'Species'] <- 'set0sa'
+unique(occurrencesLessThan(x))
+
